0f881b74012bca282379410d39a442e87fbde550
Kevin <kevinglennon01@gmail.com>
2025-03-02 01:42:19 +0000

    Starting admin dashboard

--- frontend/src/pages/AdminDashboard.tsx
+++ frontend/src/pages/AdminDashboard.tsx
@@ -1,7 +1,18 @@
+import Style from "../styles/adminDashboard.module.scss";
+
 function AdminDashboard() {
   return (
     <>
-      <div>Admin Dashboard</div>
+      <div className={Style.adminDashboardContainer}>
+        <div className={Style.adminDashboardTH}>
+          <div className={Style.tableHeader}>Users</div>
+          <div className={Style.tableHeader}>Rooms</div>
+          <div className={Style.tableHeader}>Bookings</div>
+        </div>
+        <div>
+          <div className={Style.adminDashboardTR}></div>
+        </div>
+      </div>
     </>
   );
 }
