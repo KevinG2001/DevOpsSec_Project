801c160eb60b0baac1a6fa2d6ee1432fd6813a36
Kevin <kevinglennon01@gmail.com>
2025-03-02 23:36:07 +0000

    Fixed navbar build errors

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,35 +1,37 @@
 import { useNavigate } from "react-router-dom";
 import Style from "../styles/navbar.module.scss";
-import { useState } from "react";
-import { jwtDecode } from "jwt-decode";
+import { jwtDecode, JwtPayload } from "jwt-decode";
+
+interface DecodedToken extends JwtPayload {
+  isAdmin?: boolean;
+}
 
 function Navbar() {
   const navigate = useNavigate();
-  const [selectedLink, setSelectedLink] = useState("");
-
-  //Function to get the text inside the navLink div (It also removes the space between admin dashboard)
-  //Then go navigate to that link
-  const goToLink = (event) => {
-    const link = event?.target.innerText.toLowerCase().split(" ").join("");
-    setSelectedLink(link);
-    navigate(`/${link}`);
-  };
 
   // Get the token from localStorage
   const token = localStorage.getItem("token");
   let isAdmin = false;
 
-  //Checking if the token is valid
+  // Check if the token is valid
   if (token) {
     try {
-      const decoded = jwtDecode(token);
-      isAdmin = decoded.isAdmin;
+      const decoded = jwtDecode<DecodedToken>(token);
+      isAdmin = decoded.isAdmin || false;
     } catch (error) {
       console.error("Invalid token:", error);
     }
   }
 
-  //Removes the token from local storage and navigates to the login screen
+  const goToLink = (event: React.MouseEvent<HTMLDivElement>) => {
+    const link = event.currentTarget.innerText
+      .toLowerCase()
+      .split(" ")
+      .join("");
+    navigate(`/${link}`);
+  };
+
+  // Function to log out
   const logout = () => {
     localStorage.removeItem("token");
     navigate("/");
