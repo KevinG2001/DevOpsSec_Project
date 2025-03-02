6867220fb29a098c3d414ba70b986f2ea397c09d
Kevin <kevinglennon01@gmail.com>
2025-03-02 15:10:44 +0000

    Made custom types

---
+++ frontend/src/util/types.tsx
@@ -0,0 +1,23 @@
+export interface User {
+  userid: number;
+  username: string;
+  email: string;
+  isadmin: boolean;
+}
+
+export interface Room {
+  roomid: number;
+  roomname: string;
+  roomdescription: string;
+  roomprice: number;
+  roomurl: string;
+}
+
+export interface Booking {
+  bookingid: number;
+  userid: number;
+  firstname: string;
+  roomid: number;
+  datestart: string;
+  dateend: string;
+}
